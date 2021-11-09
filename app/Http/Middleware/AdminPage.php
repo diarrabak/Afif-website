7cdcb1e30b30bef948a50208057592920c7c379f
diarrabak <71572448+diarrabak@users.noreply.github.com>
2021-11-08 19:40:35 -0500

    Add comments

--- app/Http/Middleware/AdminPage.php
+++ app/Http/Middleware/AdminPage.php
@@ -16,9 +16,9 @@ class AdminPage
      */
     public function handle(Request $request, Closure $next)
     {
-        {
+        if (in_array('admin', $request->session()->get('roles'))) {  //Only logged in admin can access the routes protected by this middleware
             return $next($request);
         }
+        return redirect()->route('home');    //Other users are redirected to the home page
     }
-    
 }
